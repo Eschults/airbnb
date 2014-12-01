1496cc2af1dfa2d25275f9c035d4b9d1999eb828
Edward Schults <edward.schults@gmail.com>
2014-12-01 21:04:20 +0100

    Flat new edit _form show

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -16,7 +16,6 @@ class FlatsController < ApplicationController
   def create
     @flat = Flat.new(flat_params)
     @flat.owner = current_user
-
     if @flat.save
       redirect_to flat_path(@flat)
     else
