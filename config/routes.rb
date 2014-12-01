09403575928c5e7c6b9bbf5bd21d0cad7ae93ab2
Edward Schults <edward.schults@gmail.com>
2014-12-01 15:53:05 +0100

    router setup done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :flats, only: [:new, :create, :show, :edit, :update] do
+    resources :bookings, only: [:new, :create]
+    resources :reviews, only: [:new, :create]
+  end
+
+  resources :users, only: [:show]
+
+  resources :bookings, only: [:show] do
+    member do
+      post :answer
+    end
+  end
+
   root to: "flats#index"
 end
