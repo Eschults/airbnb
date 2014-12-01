ba6777283a2011c99fb272f010aa89ac602771a4
Edward Schults <edward.schults@gmail.com>
2014-12-01 19:05:11 +0100

    router correction

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: "users/sessions" }
+  devise_for :users
 
   resources :flats, only: [:new, :create, :show, :edit, :update] do
     resources :bookings, only: [:new, :create]
