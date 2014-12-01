beda069f3438b48a8a68a2d621deb88bb38c88f7
Edward Schults <edward.schults@gmail.com>
2014-12-01 17:45:23 +0100

    users controller added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'users/show'
+
+  devise_for :users, controllers: { sessions: "users/sessions" }
 
   resources :flats, only: [:new, :create, :show, :edit, :update] do
     resources :bookings, only: [:new, :create]
