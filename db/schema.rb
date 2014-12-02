2cdc735042cf6cb8de22ef727ad0c2e6eb2a49ff
Florian Boret <florianboret@gmail.com>
2014-12-02 15:44:24 +0100

    facebook connect setup

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141201152143) do
+ActiveRecord::Schema.define(version: 20141202133335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,6 +96,12 @@ ActiveRecord::Schema.define(version: 20141201152143) do
     t.date     "birthday"
     t.string   "phone_number"
     t.text     "description"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "picture"
+    t.string   "name"
+    t.string   "token"
+    t.datetime "token_expiry"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
