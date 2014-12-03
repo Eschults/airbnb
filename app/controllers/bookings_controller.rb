81e4d866d3586b373d65569096e645e8197264bf
Florian Boret <florianboret@gmail.com>
2014-12-03 16:31:08 +0100

    booking controller set up

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,42 @@
+class BookingsController < ApplicationController
+before_action :set_booking, only: [:show, :answer]
+
+
+  def new
+    set_flat
+    @booking = Booking.new
+  end
+
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.flat = set_flat
+    if @booking.save
+      redirect_to flat_path(set_flat)
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def answer
+  end
+
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :flat_id, :user_id)
+  end
+
+  def set_flat
+    @flat = Flat.find(params[:flat_id])
+  end
+
+end
