3de94532e66e71b23bd72104919a33a8f82463fe
Edward Schults <edward.schults@gmail.com>
2014-12-04 19:40:49 +0100

    thursday night

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,14 +7,27 @@ before_action :set_booking, only: [:show, :edit, :update]
   end
 
   def create
-    @booking = Booking.new(booking_params)
-    @booking.flat = set_flat
-    @booking.user = current_user
-    if @booking.save
-      redirect_to flat_booking_path(@booking.flat, @booking)
-    else
-      render :new
-    end
+    # set_flat
+    # @start_date = params[:booking][:start_date]
+    # @end_date = params[:booking][:end_date]
+    # if @flat.bookings.first
+    #   @flat.bookings.each do |booking|
+    #     if (@start_date >= booking.start_date && @start_date <= booking.end_date) || (@end_date >= booking.start_date && @end_date <= booking.end_date)
+    #       alert = "This flat is not available on the requested period"
+    #       break
+    #     else
+    #     end
+    #   end
+    # else
+      @booking = Booking.new(booking_params)
+      @booking.flat = @flat
+      @booking.user = current_user
+      if @booking.save
+        redirect_to flat_booking_path(@booking.flat, @booking)
+      else
+        render :new
+      end
+    # end
   end
 
   def show
