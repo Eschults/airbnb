6fad4fefc45dba9fb8e20636b1922fa41a71ef25
Edward Schults <edward.schults@gmail.com>
2014-12-05 16:31:28 +0100

    bundle installed

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,13 @@
 class Booking < ActiveRecord::Base
+  after_create :send_book_email
   belongs_to :flat
   belongs_to :user
 
   validates_presence_of :user, :flat, :start_date, :end_date
+
+  private
+
+  def send_book_email
+    BookingMailer.book(self).deliver
+  end
 end
